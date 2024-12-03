b99fbae74a8c6c50108174ef951d0c77dad83182
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2024-12-03 23:26:14 +0100

    update: splitted Axis enum for fast type time

--- src/lean.zig
+++ src/lean.zig
@@ -2,4 +2,6 @@ const leansource = @import("leansrc.zig");
 
 pub const BasedValue = leansource.BasedValue;
 pub const LeanErrors = leansource.LeanErrors;
-pub const Axis = leansource.Axis;
+
+pub const Columns = leansource.Axis.Columns;
+pub const Rows = leansource.Axis.Rows;
