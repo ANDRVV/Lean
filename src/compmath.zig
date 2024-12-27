99227f5af60a197a0d5a7aad4d4bfdc8bba1c01e
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2024-12-27 18:42:06 +0100

    update, add: descr
    updated performance of calculation and code
    added FIxed calculation method
    added process func for single threading

--- src/compmath.zig
+++ src/compmath.zig
@@ -1,84 +1,114 @@
 const std = @import("std");
 const Operations = @import("leansrc.zig").Operations;
+const Devices = @import("leansrc.zig").Devices;
 
-/// CPUProc is a function that performs matrix basic operations on the CPU.
-pub fn CPUProc(
-    comptime T: type,
-    comptime operation: Operations,
-    comptime safeMode: bool,
-    allocator: std.mem.Allocator,
-    mat1: []const []const T,
-    mat2: []const []const T
-) ![][]T {
-    // Disable runtime safety on this block to increase performance.
-    @setRuntimeSafety(false);
-    // Set the floating point mode to hint to the LLVM
-    // to optimize the calculation removing the overhead.
-    @setFloatMode(.optimized);
-
-    const rows: usize = mat1.len;
-    const columns: usize = mat1[0].len;
-    if (rows != mat2.len or columns != mat2[0].len) return error.UnmatchedScheme;
-
+inline fn GetCompFunc(comptime T: type, comptime mode: Devices, comptime operation: Operations) fn (comptime type, T, T) callconv(.Inline) T {
     // Select the CPU class to use based on the safeMode.
-    const deviceMode: type = comptime if (safeMode) SafeCPUDevice else FastCPUDevice;
-    // Select the operation to perform based on the operation.
-    const calcFunc: fn (comptime type, T, T) callconv(.Inline) T = comptime switch (operation) {
+    const deviceMode: type = switch (mode) {
+        .SingleThreaded => |st_mode| switch (st_mode) {
+            .FastCPU => FastCPUDevice,
+            .FixedCPU => FixedCPUDevice,
+            .SafeCPU => SafeCPUDevice
+        },
+
+        .MultiThreaded => |mt_mode| switch (mt_mode) {
+            .FastCPU => FastCPUDevice,
+            .FixedCPU => FixedCPUDevice,
+            .SafeCPU => SafeCPUDevice
+        }
+    };
+
+    // Select the operation to perform calculation.
+    return switch (operation) {
         Operations.Add => deviceMode.Add,
         Operations.Sub => deviceMode.Sub,
         Operations.Mul => deviceMode.Mul,
         Operations.Div => deviceMode.Div,
     };
+}  
 
-    // Preallocate the memory for the new matrix.
-    var matrix: [][]T = try allocator.alloc([]T, rows);
-    errdefer allocator.free(matrix);
+/// CPUProcST is a function that performs matrix basic operations on the CPU without threads.
+pub fn CPUProcST(
+    comptime T: type,
+    comptime operation: Operations,
+    comptime mode: Devices,
+    allocator: std.mem.Allocator,
+    mat1: []const []T,
+    mat2: []const []const T
+) ![][]T {
+    comptime {
+        const isFastMode: bool = switch (mode) {
+            .SingleThreaded => mode.SingleThreaded,
+            .MultiThreaded => mode.MultiThreaded
+        } == .FastCPU;
 
-    // Preallocate the memory for the rows field.
-    var rowsfield: []T = try allocator.alloc(T, rows * columns);
-    errdefer allocator.free(rowsfield);
-
-    for (0..rows) |rowIndex| {
-        // Add capacity to a row with a chunk
-        matrix[rowIndex] = rowsfield[
-            rowIndex * columns..
-            rowIndex * columns + columns
-        ];
-
-        for (0..columns) |columnIndex| {
-            // Calculate and substitute the calculed value in the matrix.
-            matrix[rowIndex][columnIndex] = calcFunc(
-                T,
-                mat1[rowIndex][columnIndex],
-                mat2[rowIndex][columnIndex]
-            );
+        if (isFastMode) {
+            // Disable runtime safety on this block to increase performance.
+            @setRuntimeSafety(false);
+            // Set the floating point mode to hint to the LLVM
+            // to optimize the calculation removing the overhead.
+            @setFloatMode(.optimized);
         }
     }
 
+    // Get the operation to perform calculation.
+    const calcFunc: fn (comptime type, T, T) callconv(.Inline) T = comptime GetCompFunc(T, mode, operation);
+
+    const rows: usize = mat1.len;
+    const columns: usize = mat1[0].len;
+    if (rows != mat2.len or columns != mat2[0].len) return error.UnmatchedScheme;
+
+    // Preallocate the memory for the new matrix.
+    const matrix: [][]T = try allocator.alloc([]T, rows);
+    errdefer allocator.free(matrix);
+
+    for (0..rows) |rowIndex| {
+        // Get the row of the matrix
+        var matrixAR: []T = matrix[rowIndex];
+
+        // Add capacity to a row with a chunk
+        matrixAR = try allocator.alloc(T, columns);
+        errdefer {
+            allocator.free(matrix);
+            allocator.free(matrixAR);
+        }
+        
+        // Get alias of the row of the mat1 and mat2
+        const mat1AR: []T = mat1[rowIndex];
+        const mat2AR: []const T = mat2[rowIndex];
+
+        for (0..columns) |columnIndex| {
+            // Calculate and substitute the calculed value in the matrix.
+            matrixAR[columnIndex] = calcFunc(
+                T,
+                mat1AR[columnIndex],
+                mat2AR[columnIndex]
+            );
+        }
+    }
+    
     return matrix;
 }
 
 /// SafeCPUDevice is a struct that contains CPU operations with safe mode.
 pub const SafeCPUDevice = struct {
+
     /// Add is a function that adds two values with overflow checking.
     pub inline fn Add(comptime T: type, a: T, b: T) T { 
-        if (T == comptime_int) return a + b;
-        const ov = @addWithOverflow(a, b);
-        if (ov[1] == 0) return ov[0] else @panic("Add overflow.");
+        const v: T, const ov: u1 = @addWithOverflow(a, b);
+        if (ov == 0) return v else @panic("Add overflow.");
     }
 
     /// Sub is a function that subtracts two values with overflow checking.
     pub inline fn Sub(comptime T: type, a: T, b: T) T { 
-        if (T == comptime_int) return a - b;
-        const ov = @subWithOverflow(a, b);
-        if (ov[1] == 0) return ov[0] else @panic("Sub overflow.");
+        const v: T, const ov: u1 = @subWithOverflow(a, b);
+        if (ov == 0) return v else @panic("Sub overflow.");
     }
 
     /// Mul is a function that multiplies two values with overflow checking.
     pub inline fn Mul(comptime T: type, a: T, b: T) T {
-        if (T == comptime_int) return a * b;
-        const ov = @mulWithOverflow(a, b);
-        if (ov[1] == 0) return ov[0] else @panic("Mul overflow.");
+        const v: T, const ov: u1 = @mulWithOverflow(a, b);
+        if (ov == 0) return v else @panic("Mul overflow.");
     }
 
     /// Div is a function that divides two values with division by zero checking and type match.
@@ -90,6 +120,7 @@ pub const SafeCPUDevice = struct {
 
 /// FastCPUDevice is a struct that contains CPU operations with fast mode.
 pub const FastCPUDevice = struct {
+
     /// Add is a function that make wrapping addition.
     pub inline fn Add(comptime T: type, a: T, b: T) T { return a +| b; }
 
@@ -101,4 +132,29 @@ pub const FastCPUDevice = struct {
     
     /// Div is a function that divides 2 values if b is not equal to 0, else return the undivided value.
     pub inline fn Div(comptime T: type, a: T, b: T) T { return if (b == 0) a else a / b; }
+};
+
+/// FastCPUDevice is a struct that contains CPU operations with fixed mode.
+pub const FixedCPUDevice = struct {
+
+    /// Add is a function that adds two values: return not-added value if there is an error.
+    pub inline fn Add(comptime T: type, a: T, b: T) T { 
+        const v: T, const ov: u1 = @addWithOverflow(a, b);
+        return if (ov == 0) v else a;
+    }
+
+    /// Sub is a function that subtracts two values: return not-subtracted value if there is an error.
+    pub inline fn Sub(comptime T: type, a: T, b: T) T { 
+        const v: T, const ov: u1 = @subWithOverflow(a, b);
+        return if (ov == 0) v else a;
+    }
+
+    /// Mul is a function that multiplies two values: return not-multiplied value if there is an error.
+    pub inline fn Mul(comptime T: type, a: T, b: T) T {
+        const v: T, const ov: u1 = @mulWithOverflow(a, b);
+        return if (ov == 0) v else a;
+    }
+
+    /// Div is a function that divides two values: return not-divided value if there is an error.
+    pub const Div = FastCPUDevice.Div;
 };
