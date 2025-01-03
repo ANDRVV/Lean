152a03fdd7f84b046ad171c30c911d22950cdc87
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2025-01-03 02:27:08 +0100

    fix, add: fixed functions, added check and gen funcs

--- src/common.zig
+++ src/common.zig
@@ -6,23 +6,83 @@
 const std = @import("std");
 
 pub fn DeepClone(
-    allocator: std.mem.Allocator,
     comptime T: type,
-    src: std.ArrayList([]T)
-) !std.ArrayListAligned([]T, null) {
-    var result = std.ArrayList([]T).init(allocator);
-    for (src.items) |row| {
-        const newRow = try allocator.alloc(T, row.len);
-        for (row, 0..) |item, i| {
-            newRow[i] = item;
+    allocator: std.mem.Allocator,
+    matrix: []const []const T
+) ![][]T {
+    const rows = matrix.len;
+    const columns = matrix[0].len;
+
+    const newMatrix: [][]T = try allocator.alloc([]T, rows);
+    errdefer allocator.free(newMatrix);
+
+    for (0..rows) |rowIndex| {
+        newMatrix[rowIndex] = try allocator.alloc(T, columns);
+        errdefer {
+            allocator.free(newMatrix);
+            allocator.free(newMatrix[rowIndex]);
         }
-        try result.append(newRow);
+
+        for (0..columns) |columnIndex| {
+            newMatrix[rowIndex][columnIndex] = matrix[rowIndex][columnIndex];
+        }
     }
-    return result;
+
+    return newMatrix;
+}
+
+pub fn GenMatrixWithScheme(
+    comptime T: type,
+    allocator: std.mem.Allocator,
+    rows: usize,
+    columns: usize
+) ![][]T {
+    const matrix: [][]T = try allocator.alloc([]T, rows);
+    errdefer allocator.free(matrix);
+
+    for (0..rows) |rowIndex| {
+        // Add capacity to a row with a chunk
+        matrix[rowIndex] = try allocator.alloc(T, columns);
+        errdefer {
+            allocator.free(matrix);
+            allocator.free(matrix[rowIndex]);
+        }
+
+        for (0..columns) |columnIndex| {
+            matrix[rowIndex][columnIndex] = 0;
+        }
+    }
+
+    return matrix;
+}
+
+pub fn GenIdentityMatrix(
+    comptime T: type,
+    allocator: std.mem.Allocator,
+    rows: usize,
+    columns: usize
+) ![][]T {
+    const matrix: [][]T = try GenMatrixWithScheme(T, allocator, rows, columns);
+
+    for (0..rows) |rowIndex| {
+        for (0..columns) |columnIndex| {
+            if (rowIndex == columnIndex) matrix[rowIndex][columnIndex] = 1;
+        }
+    }
+
+    return matrix;
 }
 
 pub fn GetOptimalCapacity(cap: usize) usize {
     var new: usize = cap;
     while (new <= cap) new +|= new / 2 + 8;
     return new;
+}
+
+pub fn isUnsigned(comptime T: type) bool {
+    return switch (@typeInfo(T)) {
+       .Int => |i| i.signedness == .unsigned,
+       .Float => false,
+       else => @compileError("Invalid type."),
+    };
 }
