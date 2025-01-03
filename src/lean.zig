cf8914047637bfebbc054d1ca7a7a1c9365999c4
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2025-01-03 02:26:16 +0100

    add: added public obj

--- src/lean.zig
+++ src/lean.zig
@@ -4,8 +4,10 @@
 // See http://opensource.org/licenses/MIT
 
 const leansource = @import("leansrc.zig");
+const stdlean = @import("common.zig");
 
 pub const BasedValue = leansource.BasedValue;
+pub const Linalg = leansource.Linalg;
 pub const LeanErrors = leansource.LeanErrors;
 
 pub const Columns = leansource.Axis.Columns;
@@ -14,3 +16,5 @@ pub const Rows = leansource.Axis.Rows;
 pub const Stats = leansource.Stats;
 pub const Operations = leansource.Operations;
 pub const Devices = leansource.Devices;
+
+pub const GenIdentityMatrix = stdlean.GenIdentityMatrix;
