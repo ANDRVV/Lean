a3cbd12ec3e9a102392ce055e0648f9fd23b4bed
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2025-04-07 20:19:46 +0200

    update: updated test example

--- test.zig
+++ test.zig
@@ -2,18 +2,35 @@ const std = @import("std");
 const Lean = @import("src/lean.zig");
 
 pub fn main() !void {
-    const lean = Lean.BasedValue(i32);
-    
-    const matrix = [_][]const i32{
-        &[_]i32{1, 2, 3, 9}, 
-        &[_]i32{2, 7, 2, 2}
-    };
+    var ar = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const al = ar.allocator();
 
-    try lean.Set(&matrix);
-    try lean.Insert(&matrix);
+    const dev = Lean.Devices{.SingleThreaded  = .FastCPU};
+
+    const g: type = f64;
+
+    var lean = Lean.BasedValue(g, dev).init(al);
+    var lean2 = Lean.BasedValue(g, dev).init(al);
+
+    var linalg = Lean.Linalg(g, dev, al);
+
+    const mat1 = &[_][]const g{
+        &[_]g{2, 2, 5},
+        &[_]g{4, 5, 6},
+        &[_]g{5, 2, 2},
+    };
     
-    std.debug.print("{any}", .{lean.content()});
+    try lean.Set(mat1);
+
+    try lean.quickprint();
+    std.debug.print("\n", .{});
+
+    const a = try linalg.Transpose(lean.content());
     
+    try lean.Set(a);
+
+    try lean.quickprint();
 
     lean.Destroy();
+    lean2.Destroy();
 }
